07d11d1e9214bb6dd5236acc940ebf587809bd91
pacman-admin <getpacman.gq@gmail.com>
2024-08-29 15:14:34 -0400

    Reformatted code; recoded minor things; updated code to more recent versions of Java.

--- src/core/ipc/IPCServiceName.java
+++ src/core/ipc/IPCServiceName.java
@@ -5,14 +5,9 @@ import core.languageHandler.Language;
 public enum IPCServiceName {
     CONTROLLER_SERVER(0, "controller_server"), CLI_SERVER(1, "cli_server"), WEB_UI_SERVER(2, "web_ui_server"), PYTHON(3, Language.PYTHON.toString()), CSHARP(4, Language.CSHARP.toString()),
     ;
-
     private final int index;
     private final String name;
-
-    /**
-     * @param index
-     */
-    private IPCServiceName(final int index, final String name) {
+    IPCServiceName(final int index, final String name) {
         this.index = index;
         this.name = name;
     }
