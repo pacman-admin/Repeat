5122e7275bd9ef233555ed5024b8e53eba818a61
langdon <getpacman.gq@gmail.com>
2025-07-27 17:13:35 -0400

    Removed C# stuff.

--- src/core/languageHandler/compiler/DynamicCompilerManager.java
+++ src/core/languageHandler/compiler/DynamicCompilerManager.java
@@ -1,12 +1,22 @@
+/**
+ * Copyright 2025 Langdon Staab
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @author Langdon Staab
+ * @author HP Truong
+ */
 package core.languageHandler.compiler;
-
-import java.io.File;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 import argo.jdom.JsonNode;
 import argo.jdom.JsonNodeFactories;
@@ -16,79 +26,81 @@ import core.languageHandler.Language;
 import utilities.FileUtility;
 import utilities.json.IJsonable;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class DynamicCompilerManager implements IJsonable {
 
-	private static final Logger LOGGER = Logger.getLogger(DynamicCompilerManager.class.getName());
-	private final Map<Language, AbstractNativeCompiler> compilers;
-	private RemoteRepeatsCompilerConfig remoteRepeatsCompilerConfig;
+    private static final Logger LOGGER = Logger.getLogger(DynamicCompilerManager.class.getName());
+    private final Map<Language, AbstractNativeCompiler> compilers;
+    private RemoteRepeatsCompilerConfig remoteRepeatsCompilerConfig;
 
-	public DynamicCompilerManager() {
-		compilers = new HashMap<>();
-		compilers.put(Language.JAVA, new JavaNativeCompiler("CustomAction", new String[]{"core"}, new String[]{}));
-		//compilers.put(Language.PYTHON, new PythonRemoteCompiler(new File("core")));
-		//compilers.put(Language.CSHARP, new CSharpRemoteCompiler(new File("core")));
-		compilers.put(Language.MANUAL_BUILD, new ManualBuildNativeCompiler(new File("core")));
+    public DynamicCompilerManager() {
+        compilers = new HashMap<>();
+        compilers.put(Language.JAVA, new JavaNativeCompiler("CustomAction", new String[]{"core"}, new String[]{}));
+        //compilers.put(Language.PYTHON, new PythonRemoteCompiler(new File("core")));
+        //compilers.put(Language.CSHARP, new CSharpRemoteCompiler(new File("core")));
+        compilers.put(Language.MANUAL_BUILD, new ManualBuildNativeCompiler(new File("core")));
 
-		remoteRepeatsCompilerConfig = new RemoteRepeatsCompilerConfig(new ArrayList<>());
-	}
+        remoteRepeatsCompilerConfig = new RemoteRepeatsCompilerConfig(new ArrayList<>());
+    }
 
-	public AbstractNativeCompiler getNativeCompiler(Language name) {
-		return compilers.get(name);
-	}
+    public AbstractNativeCompiler getNativeCompiler(Language name) {
+        return compilers.get(name);
+    }
 
-	public AbstractNativeCompiler getNativeCompiler(String name) {
-		return getNativeCompiler(Language.identify(name));
-	}
+    public AbstractNativeCompiler getNativeCompiler(String name) {
+        return getNativeCompiler(Language.identify(name));
+    }
 
-	public boolean hasCompiler(Language name) {
-		return compilers.containsKey(name);
-	}
+    public boolean hasCompiler(Language name) {
+        return compilers.containsKey(name);
+    }
 
-	public RemoteRepeatsCompiler getRemoteRepeatsCompiler(RepeatsPeerServiceClientManager peerServiceClientManager) {
-		return new RemoteRepeatsCompiler(remoteRepeatsCompilerConfig, peerServiceClientManager);
-	}
+    public RemoteRepeatsCompiler getRemoteRepeatsCompiler(RepeatsPeerServiceClientManager peerServiceClientManager) {
+        return new RemoteRepeatsCompiler(remoteRepeatsCompilerConfig, peerServiceClientManager);
+    }
 
-	public RemoteRepeatsCompilerConfig getRemoteRepeatsCompilerConfig() {
-		return remoteRepeatsCompilerConfig;
-	}
+    public RemoteRepeatsCompilerConfig getRemoteRepeatsCompilerConfig() {
+        return remoteRepeatsCompilerConfig;
+    }
 
-	@Override
-	public JsonRootNode jsonize() {
-		List<JsonNode> compilerList = new ArrayList<>();
-		for (AbstractNativeCompiler compiler :  compilers.values()) {
-			compilerList.add(JsonNodeFactories.object(
-					JsonNodeFactories.field("name", JsonNodeFactories.string(compiler.getName().toString())),
-					JsonNodeFactories.field("path", JsonNodeFactories.string(FileUtility.getRelativePwdPath(compiler.getPath()))),
-					JsonNodeFactories.field("compiler_specific_args", compiler.getCompilerSpecificArgs())
-					));
-		}
+    @Override
+    public JsonRootNode jsonize() {
+        List<JsonNode> compilerList = new ArrayList<>();
+        for (AbstractNativeCompiler compiler : compilers.values()) {
+            compilerList.add(JsonNodeFactories.object(JsonNodeFactories.field("name", JsonNodeFactories.string(compiler.getName().toString())), JsonNodeFactories.field("path", JsonNodeFactories.string(FileUtility.getRelativePwdPath(compiler.getPath()))), JsonNodeFactories.field("compiler_specific_args", compiler.getCompilerSpecificArgs())));
+        }
 
-		return JsonNodeFactories.object(
-				JsonNodeFactories.field("local_compilers", JsonNodeFactories.array(compilerList)),
-				JsonNodeFactories.field("remote_repeats_compilers", remoteRepeatsCompilerConfig.jsonize()));
-	}
+        return JsonNodeFactories.object(JsonNodeFactories.field("local_compilers", JsonNodeFactories.array(compilerList)), JsonNodeFactories.field("remote_repeats_compilers", remoteRepeatsCompilerConfig.jsonize()));
+    }
 
-	public boolean parseJSON(JsonNode compilerSettings) {
-		JsonNode remoteRepeatsCompilers = compilerSettings.getNode("remote_repeats_compilers");
-		RemoteRepeatsCompilerConfig remoteCompilers = RemoteRepeatsCompilerConfig.parseJSON(remoteRepeatsCompilers);
-		remoteRepeatsCompilerConfig.setClients(remoteCompilers.getClients());
+    public boolean parseJSON(JsonNode compilerSettings) {
+        JsonNode remoteRepeatsCompilers = compilerSettings.getNode("remote_repeats_compilers");
+        RemoteRepeatsCompilerConfig remoteCompilers = RemoteRepeatsCompilerConfig.parseJSON(remoteRepeatsCompilers);
+        remoteRepeatsCompilerConfig.setClients(remoteCompilers.getClients());
 
-		List<JsonNode> localCompilers = compilerSettings.getArrayNode("local_compilers");
-		for (JsonNode compilerNode : localCompilers) {
-			String name = compilerNode.getStringValue("name");
-			String path = compilerNode.getStringValue("path");
-			JsonNode compilerSpecificArgs = compilerNode.getNode("compiler_specific_args");
+        List<JsonNode> localCompilers = compilerSettings.getArrayNode("local_compilers");
+        for (JsonNode compilerNode : localCompilers) {
+            String name = compilerNode.getStringValue("name");
+            String path = compilerNode.getStringValue("path");
+            JsonNode compilerSpecificArgs = compilerNode.getNode("compiler_specific_args");
 
-			AbstractNativeCompiler compiler = getNativeCompiler(name);
-			if (compiler != null) {
-				compiler.setPath(new File(path));
-				if (!compiler.parseCompilerSpecificArgs(compilerSpecificArgs)) {
-					LOGGER.log(Level.WARNING, "Compiler " + name + " was unable to parse its specific arguments.");
-				}
-			} else {
-				throw new IllegalStateException("Unknown compiler " + name);
-			}
-		}
-		return true;
-	}
+            AbstractNativeCompiler compiler = getNativeCompiler(name);
+            if (compiler != null) {
+                compiler.setPath(new File(path));
+                if (!compiler.parseCompilerSpecificArgs(compilerSpecificArgs)) {
+                    LOGGER.log(Level.WARNING, "Compiler " + name + " was unable to parse its specific arguments.");
+                }
+            } else {
+                throw new IllegalStateException("Unknown compiler " + name);
+            }
+        }
+        return true;
+    }
 }
--- src/staticResources/BootStrapResources.java
+++ src/staticResources/BootStrapResources.java
@@ -1,6 +1,30 @@
+/**
+ * Copyright 2025 Langdon Staab
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @author Langdon Staab
+ * @author HP Truong
+ */
 package staticResources;
 
-import java.awt.Image;
+import core.config.Config;
+import core.languageHandler.Language;
+import utilities.FileUtility;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URISyntaxException;
@@ -11,115 +35,102 @@ import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import javax.imageio.ImageIO;
-import javax.swing.ImageIcon;
-
-import core.config.Config;
-import core.languageHandler.Language;
-import utilities.FileUtility;
-
 public class BootStrapResources {
 
-	private static final Logger LOGGER = Logger.getLogger(BootStrapResources.class.getName());
+    public static final String SOUND_POSITIVE1_PATH = "/staticContent/sounds/notifications/positive1.wav";
+    public static final String SOUND_POSITIVE2_PATH = "/staticContent/sounds/notifications/positive2.wav";
+    public static final String SOUND_POSITIVE3_PATH = "/staticContent/sounds/notifications/positive3.wav";
+    public static final String SOUND_POSITIVE4_PATH = "/staticContent/sounds/notifications/positive4.wav";
+    public static final String SOUND_NEGATIVE1_PATH = "/staticContent/sounds/notifications/negative1.wav";
+    public static final String SOUND_NEGATIVE2_PATH = "/staticContent/sounds/notifications/negative2.wav";
+    public static final Image TRAY_IMAGE;
+    private static final Logger LOGGER = Logger.getLogger(BootStrapResources.class.getName());
+    private static final Map<Language, String> LANGUAGE_API;
+    private static final Map<Language, String> NATIVE_LANGUAGE_TEMPLATES;
+    private static final Map<Language, AbstractNativeLanguageBootstrapResource> NATIVE_BOOTSTRAP_RESOURCES;
+    private static final Set<BootstrapResourcesExtrator> BOOTSTRAP_RESOURCES;
+    private static final NativeHookBootstrapResources nativeHookResources;
 
-	private static final Map<Language, String> LANGUAGE_API;
-	private static final Map<Language, String> NATIVE_LANGUAGE_TEMPLATES;
-	private static final Map<Language, AbstractNativeLanguageBootstrapResource> NATIVE_BOOTSTRAP_RESOURCES;
-	private static final Set<BootstrapResourcesExtrator> BOOTSTRAP_RESOURCES;
+    static {
+        TRAY_IMAGE = getImage("/staticContent/Repeat.jpg");
 
-	public static final String SOUND_POSITIVE1_PATH = "/staticContent/sounds/notifications/positive1.wav";
-	public static final String SOUND_POSITIVE2_PATH = "/staticContent/sounds/notifications/positive2.wav";
-	public static final String SOUND_POSITIVE3_PATH = "/staticContent/sounds/notifications/positive3.wav";
-	public static final String SOUND_POSITIVE4_PATH = "/staticContent/sounds/notifications/positive4.wav";
-	public static final String SOUND_NEGATIVE1_PATH = "/staticContent/sounds/notifications/negative1.wav";
-	public static final String SOUND_NEGATIVE2_PATH = "/staticContent/sounds/notifications/negative2.wav";
+        /*********************************************************************************/
+        LANGUAGE_API = new HashMap<>();
+        LANGUAGE_API.put(Language.JAVA, getFile("/staticContent/core/languageHandler/API/JavaAPI.txt"));
+        //LANGUAGE_API.put(Language.PYTHON, getFile("/staticContent/core/languageHandler/API/PythonAPI.txt"));
+        //LANGUAGE_API.put(Language.CSHARP, getFile("/staticContent/core/languageHandler/API/CSharpAPI.txt"));
 
+        /*********************************************************************************/
+        NATIVE_LANGUAGE_TEMPLATES = new HashMap<>();
+        NATIVE_LANGUAGE_TEMPLATES.put(Language.JAVA, getFile("/staticContent/natives/java/TemplateRepeat"));
+        //NATIVE_LANGUAGE_TEMPLATES.put(Language.PYTHON, getFile("/staticContent/natives/python/template_repeat.py"));
+        //NATIVE_LANGUAGE_TEMPLATES.put(Language.CSHARP, getFile("/staticContent/natives/csharp/source/TemplateRepeat.cs"));
+        NATIVE_LANGUAGE_TEMPLATES.put(Language.MANUAL_BUILD, getFile("/staticContent/natives/manual/TemplateRepeat.txt"));
 
-	private static final NativeHookBootstrapResources nativeHookResources;
+        /*********************************************************************************/
+        NATIVE_BOOTSTRAP_RESOURCES = new HashMap<>();
+        //NATIVE_BOOTSTRAP_RESOURCES.put(Language.PYTHON, new PythonResources());
+        //NATIVE_BOOTSTRAP_RESOURCES.put(Language.CSHARP, new CSharpResources());
 
-	public static final Image TRAY_IMAGE;
+        /*********************************************************************************/
+        BOOTSTRAP_RESOURCES = new HashSet<>();
+        BOOTSTRAP_RESOURCES.addAll(NATIVE_BOOTSTRAP_RESOURCES.values());
 
-	static {
-		TRAY_IMAGE = getImage("/staticContent/Repeat.jpg");
+        nativeHookResources = new NativeHookBootstrapResources();
+        BOOTSTRAP_RESOURCES.add(nativeHookResources);
+    }
 
-		/*********************************************************************************/
-		LANGUAGE_API = new HashMap<>();
-		LANGUAGE_API.put(Language.JAVA, getFile("/staticContent/core/languageHandler/API/JavaAPI.txt"));
-		//LANGUAGE_API.put(Language.PYTHON, getFile("/staticContent/core/languageHandler/API/PythonAPI.txt"));
-		LANGUAGE_API.put(Language.CSHARP, getFile("/staticContent/core/languageHandler/API/CSharpAPI.txt"));
+    private BootStrapResources() {
+    }
 
-		/*********************************************************************************/
-		NATIVE_LANGUAGE_TEMPLATES = new HashMap<>();
-		NATIVE_LANGUAGE_TEMPLATES.put(Language.JAVA, getFile("/staticContent/natives/java/TemplateRepeat"));
-		//NATIVE_LANGUAGE_TEMPLATES.put(Language.PYTHON, getFile("/staticContent/natives/python/template_repeat.py"));
-		NATIVE_LANGUAGE_TEMPLATES.put(Language.CSHARP, getFile("/staticContent/natives/csharp/source/TemplateRepeat.cs"));
-		NATIVE_LANGUAGE_TEMPLATES.put(Language.MANUAL_BUILD, getFile("/staticContent/natives/manual/TemplateRepeat.txt"));
+    public static AbstractNativeLanguageBootstrapResource getBootstrapResource(Language language) {
+        return NATIVE_BOOTSTRAP_RESOURCES.get(language);
+    }
 
-		/*********************************************************************************/
-		NATIVE_BOOTSTRAP_RESOURCES = new HashMap<>();
-		//NATIVE_BOOTSTRAP_RESOURCES.put(Language.PYTHON, new PythonResources());
-		//NATIVE_BOOTSTRAP_RESOURCES.put(Language.CSHARP, new CSharpResources());
+    public static void extractResources() throws IOException, URISyntaxException {
+        for (BootstrapResourcesExtrator resource : BOOTSTRAP_RESOURCES) {
+            resource.extractResources();
+        }
+    }
 
-		/*********************************************************************************/
-		BOOTSTRAP_RESOURCES = new HashSet<>();
-		BOOTSTRAP_RESOURCES.addAll(NATIVE_BOOTSTRAP_RESOURCES.values());
+    protected static ImageIcon getIcon(String resource) {
+        return new ImageIcon(getImage(resource));
+    }
 
-		nativeHookResources = new NativeHookBootstrapResources();
-		BOOTSTRAP_RESOURCES.add(nativeHookResources);
-	}
+    public static InputStream getStaticContentStream(String resource) {
+        return BootStrapResources.class.getResourceAsStream(resource);
+    }
 
-	public static AbstractNativeLanguageBootstrapResource getBootstrapResource(Language language) {
-		return NATIVE_BOOTSTRAP_RESOURCES.get(language);
-	}
+    protected static Image getImage(String resource) {
+        try {
+            return ImageIO.read(BootStrapResources.class.getResourceAsStream(resource));
+        } catch (IOException e) {
+            LOGGER.log(Level.SEVERE, "Cannot load image " + resource, e);
+            return null;
+        }
+    }
 
-	public static void extractResources() throws IOException, URISyntaxException {
-		for (BootstrapResourcesExtrator resource : BOOTSTRAP_RESOURCES) {
-			resource.extractResources();
-		}
-	}
+    protected static String getFile(String path) {
+        return FileUtility.readFromStream(BootStrapResources.class.getResourceAsStream(path)).toString();
+    }
 
-	protected static ImageIcon getIcon(String resource) {
-		return new ImageIcon(getImage(resource));
-	}
+    public static String getAbout() {
+        return "Repeat " + Config.RELEASE_VERSION + "\n" + "A tool to repeat yourself with some intelligence.\n" + "Created by HP Truong. Contact me at hptruong93@gmail.com.";
+    }
 
-	public static InputStream getStaticContentStream(String resource) {
-		return BootStrapResources.class.getResourceAsStream(resource);
-	}
+    public static String getAPI(Language language) {
+        if (LANGUAGE_API.containsKey(language)) {
+            return LANGUAGE_API.get(language);
+        } else {
+            return "";
+        }
+    }
 
-	protected static Image getImage(String resource) {
-		try {
-			return ImageIO.read(BootStrapResources.class.getResourceAsStream(resource));
-		} catch (IOException e) {
-			LOGGER.log(Level.SEVERE, "Cannot load image " + resource, e);
-			return null;
-		}
-	}
-
-	protected static String getFile(String path) {
-		return FileUtility.readFromStream(BootStrapResources.class.getResourceAsStream(path)).toString();
-	}
-
-	public static String getAbout() {
-		return "Repeat " + Config.RELEASE_VERSION + "\n"
-				+ "A tool to repeat yourself with some intelligence.\n"
-				+ "Created by HP Truong. Contact me at hptruong93@gmail.com.";
-	}
-
-	public static String getAPI(Language language) {
-		if (LANGUAGE_API.containsKey(language)) {
-			return LANGUAGE_API.get(language);
-		} else {
-			return "";
-		}
-	}
-
-	public static String getNativeLanguageTemplate(Language language) {
-		if (NATIVE_LANGUAGE_TEMPLATES.containsKey(language)) {
-			return NATIVE_LANGUAGE_TEMPLATES.get(language);
-		} else {
-			return "";
-		}
-	}
-
-	private BootStrapResources() {}
+    public static String getNativeLanguageTemplate(Language language) {
+        if (NATIVE_LANGUAGE_TEMPLATES.containsKey(language)) {
+            return NATIVE_LANGUAGE_TEMPLATES.get(language);
+        } else {
+            return "";
+        }
+    }
 }
