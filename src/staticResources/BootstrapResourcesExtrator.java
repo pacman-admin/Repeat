2f4f1d86cd27a230ee3ce7736ffae1249b878f65
langdon <getpacman.gq@gmail.com>
2025-07-27 17:33:57 -0400

    Removed useless stuff and added license notice.

--- src/staticResources/BootstrapResourcesExtrator.java
+++ src/staticResources/BootstrapResourcesExtrator.java
@@ -22,5 +22,5 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 
 public interface BootstrapResourcesExtrator {
-    public void extractResources() throws IOException, URISyntaxException;
+    void extractResources() throws IOException, URISyntaxException;
 }
