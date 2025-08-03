b1ca17a5acec27cca0102c7958bcd8ae4f1e65f3
langdon <getpacman.gq@gmail.com>
2025-08-03 13:22:56 -0400

    Removed useless stuff

--- src/main/Main.java
+++ src/main/Main.java
@@ -22,13 +22,6 @@ import frontEnd.MainFrontEnd;
 
 public class Main {
     public static void main(String[] args) {
-        //if (args.length == 0) {
         MainFrontEnd.run();
-        //} else if (args[0].equals("cli")) {
-        //MainCli cli = new MainCli();
-        //String[] cliArgs = Arrays.copyOfRange(args, 1, args.length);
-        //cli.process(cliArgs);
-        //}
-        //KeyChainInputPanel.getInputKeyChain(new JFrame(), new KeyChain());
     }
 }
