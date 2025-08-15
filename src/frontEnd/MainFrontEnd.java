2e344368aadd9281bf5c9ecc9542851ca1c509be
langdon <getpacman.gq@gmail.com>
2025-08-15 16:40:46 -0400

    Removed manual edit source code feature. Nobody's gonna use it anyway.

--- src/frontEnd/MainFrontEnd.java
+++ src/frontEnd/MainFrontEnd.java
@@ -1,51 +1,52 @@
 package frontEnd;
 
+import core.userDefinedTask.internals.SharedVariablesPubSubManager;
+import globalListener.GlobalListenerHookController;
+import staticResources.BootStrapResources;
+
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import core.userDefinedTask.internals.SharedVariablesPubSubManager;
-import globalListener.GlobalListenerHookController;
-import staticResources.BootStrapResources;
-
+@SuppressWarnings("DanglingJavadoc")
 public class MainFrontEnd {
 
-	private static final Logger LOGGER = Logger.getLogger(MainFrontEnd.class.getName());
+    private static final Logger LOGGER = Logger.getLogger(MainFrontEnd.class.getName());
 
     public static void run() {
-		/*************************************************************************************/
-		/********************************Extracting resources*********************************/
-		try {
-			BootStrapResources.extractResources();
-		} catch (IOException | URISyntaxException e) {
-			LOGGER.log(Level.SEVERE, "Cannot extract bootstrap resources.", e);
-			System.exit(2);
-		}
-		/*************************************************************************************/
-		/********************************Load configs****************************/
+        /*************************************************************************************/
+        /********************************Extracting resources*********************************/
+        try {
+            BootStrapResources.extractResources();
+        } catch (IOException | URISyntaxException e) {
+            LOGGER.log(Level.SEVERE, "Cannot extract bootstrap resources.", e);
+            System.exit(2);
+        }
+        /*************************************************************************************/
+        /********************************Load configs****************************/
         MainBackEndHolder backEnd = new MainBackEndHolder();
-		backEnd.loadConfig(null);
-		/*************************************************************************************/
-		/********************************Initializing global hooks****************************/
-		GlobalListenerHookController.Config hookConfig = GlobalListenerHookController.Config.Builder.of().useJavaAwtForMousePosition(backEnd.getConfig().isUseJavaAwtToGetMousePosition()).build();
-		GlobalListenerHookController.of().initialize(hookConfig);
-		SharedVariablesPubSubManager.get().start();
-		/*************************************************************************************/
-		/********************************Start main program***********************************/
-		try {
-			backEnd.keysManager.startGlobalListener();
-		} catch (Exception e) {
-			LOGGER.warning("Could not start global listener!\n"+e.getMessage());
-		}
+        backEnd.loadConfig(null);
+        /*************************************************************************************/
+        /********************************Initializing global hooks****************************/
+        GlobalListenerHookController.Config hookConfig = GlobalListenerHookController.Config.Builder.of().useJavaAwtForMousePosition(backEnd.getConfig().isUseJavaAwtToGetMousePosition()).build();
+        GlobalListenerHookController.of().initialize(hookConfig);
+        SharedVariablesPubSubManager.get().start();
+        /*************************************************************************************/
+        /********************************Start main program***********************************/
+        try {
+            backEnd.keysManager.startGlobalListener();
+        } catch (Exception e) {
+            LOGGER.warning("Could not start global listener!\n" + e.getMessage());
+        }
 
-		backEnd.configureMainHotkeys();
-		/*************************************************************************************/
-		backEnd.renderTaskGroup();
+        backEnd.configureMainHotkeys();
+        /*************************************************************************************/
+        backEnd.renderTaskGroup();
 
-		/*************************************************************************************/
-		backEnd.initializeLogging();
-		backEnd.initiateBackEndActivities();
-		backEnd.launchUI();
-	}
+        /*************************************************************************************/
+        backEnd.initializeLogging();
+        backEnd.initiateBackEndActivities();
+        backEnd.launchUI();
+    }
 }
