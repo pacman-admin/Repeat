5288aadc5fc9b7cbed74f1d6ca142b71df1d50d5
langdon <getpacman.gq@gmail.com>
2025-08-25 14:46:26 -0400

    Made access weaker.
    Deleted garbage.

--- src/core/webui/server/handlers/internals/globalconfigs/AbstractSetRemoteRepeatsClientsHandler.java
+++ src/core/webui/server/handlers/internals/globalconfigs/AbstractSetRemoteRepeatsClientsHandler.java
@@ -1,49 +1,48 @@
 package core.webui.server.handlers.internals.globalconfigs;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-
-import org.apache.http.HttpRequest;
-import org.apache.http.nio.protocol.HttpAsyncExchange;
-import org.apache.http.protocol.HttpContext;
-
 import argo.jdom.JsonNode;
 import core.webui.server.handlers.AbstractSingleMethodHttpHandler;
 import core.webui.server.handlers.AbstractUIHttpHandler;
 import core.webui.server.handlers.renderedobjects.ObjectRenderer;
 import core.webui.webcommon.HttpServerUtilities;
+import org.apache.http.HttpRequest;
+import org.apache.http.nio.protocol.HttpAsyncExchange;
+import org.apache.http.protocol.HttpContext;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public abstract class AbstractSetRemoteRepeatsClientsHandler extends AbstractUIHttpHandler {
-	AbstractSetRemoteRepeatsClientsHandler(ObjectRenderer objectRenderer) {
-		super(objectRenderer, AbstractSingleMethodHttpHandler.POST_METHOD);
-	}
+    AbstractSetRemoteRepeatsClientsHandler(ObjectRenderer objectRenderer) {
+        super(objectRenderer, AbstractSingleMethodHttpHandler.POST_METHOD);
+    }
 
-	@Override
-	protected final Void handleAllowedRequestWithBackend(HttpRequest request, HttpAsyncExchange exchange, HttpContext context) throws IOException {
-		JsonNode params = HttpServerUtilities.parsePostParameters(request);
-		if (params == null) {
-			return HttpServerUtilities.prepareHttpResponse(exchange, 500, "Unable to get POST paramters.");
-		}
-		if (!params.isArrayNode("clients")) {
-			return HttpServerUtilities.prepareHttpResponse(exchange, 400, "Clients need to be an array of strings.");
-		}
-		List<JsonNode> clients = params.getArrayNode("clients");
-		if (clients.isEmpty()) {
-			return HttpServerUtilities.prepareHttpResponse(exchange, 400, "Must use at least one client.");
-		}
+    @Override
+    protected final Void handleAllowedRequestWithBackend(HttpRequest request, HttpAsyncExchange exchange, HttpContext context) throws IOException {
+        JsonNode params = HttpServerUtilities.parsePostParameters(request);
+        if (params == null) {
+            return HttpServerUtilities.prepareHttpResponse(exchange, 500, "Unable to get POST paramters.");
+        }
+        if (!params.isArrayNode("clients")) {
+            return HttpServerUtilities.prepareHttpResponse(exchange, 400, "Clients need to be an array of strings.");
+        }
+        List<JsonNode> clients = params.getArrayNode("clients");
+        if (clients.isEmpty()) {
+            return HttpServerUtilities.prepareHttpResponse(exchange, 400, "Must use at least one client.");
+        }
 
-		List<String> clientIds = new ArrayList<>();
-		try {
-			clientIds = clients.stream().map(JsonNode::getStringValue).distinct().collect(Collectors.toList());
-		} catch(Exception e) {
-			return HttpServerUtilities.prepareHttpResponse(exchange, 400, "Clients need to be an array of strings.");
-		}
-		setConfig(clientIds);
-		return renderResponse(exchange);
-	}
+        List<String> clientIds;
+        try {
+            clientIds = clients.stream().map(JsonNode::getStringValue).distinct().collect(Collectors.toList());
+        } catch (Exception e) {
+            return HttpServerUtilities.prepareHttpResponse(exchange, 400, "Clients need to be an array of strings.");
+        }
+        setConfig(clientIds);
+        return renderResponse(exchange);
+    }
 
-	protected abstract void setConfig(List<String> clientIds);
-	protected abstract Void renderResponse(HttpAsyncExchange exchange) throws IOException;
+    protected abstract void setConfig(List<String> clientIds);
+
+    protected abstract Void renderResponse(HttpAsyncExchange exchange) throws IOException;
 }
--- src/core/webui/server/handlers/internals/globalconfigs/SetCoreConfigClientsHandler.java
+++ src/core/webui/server/handlers/internals/globalconfigs/SetCoreConfigClientsHandler.java
@@ -1,25 +1,24 @@
 package core.webui.server.handlers.internals.globalconfigs;
 
+import core.webui.server.handlers.renderedobjects.ObjectRenderer;
+import org.apache.http.nio.protocol.HttpAsyncExchange;
+
 import java.io.IOException;
 import java.util.List;
 
-import org.apache.http.nio.protocol.HttpAsyncExchange;
-
-import core.webui.server.handlers.renderedobjects.ObjectRenderer;
-
 public class SetCoreConfigClientsHandler extends AbstractSetRemoteRepeatsClientsHandler {
 
-	public SetCoreConfigClientsHandler(ObjectRenderer objectRenderer) {
-		super(objectRenderer);
-	}
+    public SetCoreConfigClientsHandler(ObjectRenderer objectRenderer) {
+        super(objectRenderer);
+    }
 
-	@Override
-	public void setConfig(List<String> clientIds) {
-		backEndHolder.setCoreClients(clientIds);
-	}
+    @Override
+    public void setConfig(List<String> clientIds) {
+        backEndHolder.setCoreClients(clientIds);
+    }
 
-	@Override
-	public Void renderResponse(HttpAsyncExchange exchange) throws IOException {
-		return renderedCoreClientsConfig(exchange);
-	}
+    @Override
+    public Void renderResponse(HttpAsyncExchange exchange) throws IOException {
+        return renderedCoreClientsConfig(exchange);
+    }
 }
