d397e55e678e7ecf4b7016ea0d3392d0d907bfa3
langdon <getpacman.gq@gmail.com>
2025-09-01 10:13:56 -0400

    Fixed issues.

--- src/core/webui/server/handlers/internals/tasks/manuallybuild/InvalidManuallyBuildComponentException.java
+++ src/core/webui/server/handlers/internals/tasks/manuallybuild/InvalidManuallyBuildComponentException.java
@@ -1,16 +1,17 @@
 package core.webui.server.handlers.internals.tasks.manuallybuild;
 
+@SuppressWarnings("MissingSerialAnnotation")
 class InvalidManuallyBuildComponentException extends Exception {
-	private static final long serialVersionUID = 5763742112199815619L;
+    private static final long serialVersionUID = 5763742112199815619L;
 
-	private String message;
+    private final String message;
 
-	public InvalidManuallyBuildComponentException(String message) {
-		this.message = message;
-	}
+    public InvalidManuallyBuildComponentException(String message) {
+        this.message = message;
+    }
 
-	@Override
-	public String toString() {
-		return message;
-	}
+    @Override
+    public String toString() {
+        return message;
+    }
 }
--- src/utilities/OSIdentifier.java
+++ src/utilities/OSIdentifier.java
@@ -1,23 +1,15 @@
 package utilities;
 
-public class OSIdentifier {
+public enum OS{
+	WINDOWS,
+    GNU_LINUX,
+    MAC,
+    OTHER;
 
-	private static final String OS_NAME;
-	public static final boolean IS_WINDOWS;
-    private static final boolean IS_UNIX;
-    public static final boolean IS_LINUX;
-    public static final boolean IS_OSX;
-    private static final boolean IS_UNKNOWN;
-
-	static {
-		// See http://lopica.sourceforge.net/os.html
-		OS_NAME = System.getProperty("os.name").toLowerCase();
-		IS_WINDOWS = OS_NAME.startsWith("win");
-		IS_UNIX = !IS_WINDOWS;
-		IS_OSX = OS_NAME.startsWith("mac");
-		IS_LINUX = OS_NAME.startsWith("linux");
-		IS_UNKNOWN = !IS_WINDOWS && !IS_LINUX && !IS_OSX;
-	}
-
-	private OSIdentifier() {}
+	public static OS getCurrentOS(){
+        String OSName = System.getProperty("os.name");
+        if (OSName.con("")){
+            return OS.MAC;
+        }
+    }
 }
