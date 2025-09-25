d06102813c239de347296d2a18a364b486b3ced6
langdon <getpacman.gq@gmail.com>
2025-09-25 16:46:35 -0400

    Removed garbage.

--- src/core/userDefinedTask/internals/ActionExecutionRequest.java
+++ src/core/userDefinedTask/internals/ActionExecutionRequest.java
@@ -1,33 +1,28 @@
 package core.userDefinedTask.internals;
 
-import core.keyChain.TaskActivation;
-
 public class ActionExecutionRequest {
-	private int repeatCount;
-	private long delayMsBetweenRepeat;
-	private TaskActivation activation;
+    private int repeatCount;
+    private long delayMsBetweenRepeat;
+    //private TaskActivation activation;
 
-	private ActionExecutionRequest() {}
+    private ActionExecutionRequest() {
+    }
 
-	public static ActionExecutionRequest of() {
-		return of(1, 0);
-	}
+    public static ActionExecutionRequest of() {
+        return of(1, 0);
+    }
 
-	public static ActionExecutionRequest of(int repeatCount, long delay) {
-		ActionExecutionRequest result = new ActionExecutionRequest();
-		result.repeatCount = repeatCount;
-		result.delayMsBetweenRepeat = delay;
-		result.activation = TaskActivation.newBuilder().build();
-		return result;
-	}
-
-	public int getRepeatCount() {
-		return repeatCount;
-	}
-	public long getDelayMsBetweenRepeat() {
-		return delayMsBetweenRepeat;
-	}
-	public TaskActivation getActivation() {
-		return activation;
-	}
+    public static ActionExecutionRequest of(int repeatCount, long delay) {
+        ActionExecutionRequest result = new ActionExecutionRequest();
+        result.repeatCount = repeatCount;
+        result.delayMsBetweenRepeat = delay;
+        //result.activation = TaskActivation.newBuilder().build();
+        return result;
+    }
+    public int getRepeatCount() {
+        return repeatCount;
+    }
+    public long getDelayMsBetweenRepeat() {
+        return delayMsBetweenRepeat;
+    }
 }
