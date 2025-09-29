a1bb1c6070cdb21056a518e6c721b09c022cfbe2
langdon <getpacman.gq@gmail.com>
2025-09-28 21:30:19 -0400

    Removed garbage and fixed typos.

--- src/core/ipc/IPCServiceManager.java
+++ src/core/ipc/IPCServiceManager.java
@@ -2,122 +2,45 @@ package core.ipc;
 
 import argo.jdom.JsonNode;
 import argo.jdom.JsonNodeFactories;
-import core.languageHandler.Language;
 import core.webui.server.UIServer;
 import frontEnd.MainBackEndHolder;
-import utilities.Function;
 
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.logging.Logger;
 
 public final class IPCServiceManager {
-
-    public static final int IPC_SERVICE_COUNT = IPCServiceName.values().length;
-    private static final Logger LOGGER = Logger.getLogger(IPCServiceManager.class.getName());
-    private static final long INTER_SERVICE_BOOT_TIME_MS = 200;
-    private static final IIPCService[] ipcServices;
-    private static final Map<Language, Integer> ipcByLanguage;
+    private static final IIPCService UI_SERVER;
 
     static {
-        ipcServices = new IIPCService[IPC_SERVICE_COUNT];
-        ipcServices[IPCServiceName.WEB_UI_SERVER.value()] = new UIServer();
-        ipcByLanguage = new HashMap<>();
-        ipcByLanguage.put(Language.JAVA, -1);
-        ipcByLanguage.put(Language.MANUAL_BUILD, -1);
+        UI_SERVER = new UIServer();
     }
+
 
     private IPCServiceManager() {
     }
 
-    public static IIPCService getIPCService(Language name) {
-        int index = ipcByLanguage.get(name);
-        if (index >= 0) {
-            return ipcServices[index];
-        } else {
-            return null;
-        }
-    }
-
-    public static IIPCService getIPCService(IPCServiceName name) {
-        if (name == null) {
-            return null;
-        }
-        return ipcServices[name.value()];
-    }
-
-    public static IIPCService getIPCService(int index) {
-        if (index >= IPC_SERVICE_COUNT) {
-            return null;
-        }
-
-        return ipcServices[index];
-    }
-
-    public static void initiateServices(MainBackEndHolder backEndHolder) throws IOException {
-        for (IPCServiceName name : IPCServiceName.values()) {
-            IIPCService service = IPCServiceManager.getIPCService(name);
-            if (name == IPCServiceName.WEB_UI_SERVER) {
-                UIServer server = (UIServer) service;
-                server.setMainBackEndHolder(backEndHolder);
-            }
-            if (!service.isLaunchAtStartup()) {
-                continue;
-            }
-            LOGGER.fine("Starting ipc service " + service.getName());
-            service.startRunning();
-
-            try {
-                Thread.sleep(INTER_SERVICE_BOOT_TIME_MS);
-            } catch (InterruptedException ignored) {
-            }
-        }
-    }
-
     public static void stopServices() throws IOException {
-        for (int i = IPCServiceName.values().length - 1; i >= 0; i--) {
-            IPCServiceName name = IPCServiceName.values()[i];
-            IIPCService service = IPCServiceManager.getIPCService(name);
-            do {
-                service.stopRunning();
-                try {
-                    Thread.sleep(100);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            } while (service.isRunning());
-        }
+        UI_SERVER.stopRunning();
+        while (UI_SERVER.isRunning()) ;
     }
 
     public static boolean parseJSON(List<JsonNode> ipcSettings) {
-        boolean result = true;
-
         for (JsonNode language : ipcSettings) {
             String name = language.getStringValue("name");
-            IIPCService service = IPCServiceManager.getIPCService(IPCServiceName.identifyService(name));
-
-            if (service != null) {
-                boolean newResult = service.extractSpecificConfig(language.getNode("config"));
-                if (!newResult) {
-                    LOGGER.warning("Unable to parse config for ipc service " + name);
-                }
-                result &= newResult;
+            if (name.equals("web_ui_server")) {
+                return UI_SERVER.extractSpecificConfig(language.getNode("config"));
             }
         }
-
-        return result;
+        throw new RuntimeException("UI Server not found!");
     }
 
     public static JsonNode jsonize() {
-        return JsonNodeFactories.array(new Function<IPCServiceName, JsonNode>() {
-            @Override
-            public JsonNode apply(IPCServiceName n) {
-                IIPCService service = getIPCService(n);
+        return JsonNodeFactories.array(JsonNodeFactories.object(JsonNodeFactories.field("name", JsonNodeFactories.string("web_ui_server")), JsonNodeFactories.field("config", UI_SERVER == null ? JsonNodeFactories.nullNode() : UI_SERVER.getSpecificConfig())));
+    }
 
-                return JsonNodeFactories.object(JsonNodeFactories.field("name", JsonNodeFactories.string(n.toString())), JsonNodeFactories.field("config", service == null ? JsonNodeFactories.nullNode() : service.getSpecificConfig()));
-            }
-        }.map(IPCServiceName.values()));
+    public static void initiateServices(MainBackEndHolder backEndHolder) throws IOException {
+        UIServer server = (UIServer) UI_SERVER;
+        server.setMainBackEndHolder(backEndHolder);
+        UI_SERVER.startRunning();
     }
 }
