4608949dcb59d1a9f86ce449dfb266cf792c2a20
langdon <getpacman.gq@gmail.com>
2025-11-03 17:00:08 -0500

    Removed garbage.

--- src/core/userDefinedTask/manualBuild/ManuallyBuildActionConstructor.java
+++ src/core/userDefinedTask/manualBuild/ManuallyBuildActionConstructor.java
@@ -1,81 +1,81 @@
 package core.userDefinedTask.manualBuild;
 
+import core.languageHandler.compiler.ManualBuildNativeCompiler;
+import utilities.json.JSONUtility;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import core.languageHandler.compiler.ManualBuildNativeCompiler;
-import utilities.json.JSONUtility;
-
 public class ManuallyBuildActionConstructor {
 
-	private final List<ManuallyBuildStep> steps;
+    private final List<ManuallyBuildStep> steps;
 
-	public static ManuallyBuildActionConstructor of() {
-		return new ManuallyBuildActionConstructor(new ArrayList<>());
-	}
+    private ManuallyBuildActionConstructor(List<ManuallyBuildStep> steps) {
+        this.steps = steps;
+    }
 
-	public static ManuallyBuildActionConstructor of(ManuallyBuildAction action) {
-		return new ManuallyBuildActionConstructor(new ArrayList<>(action.getSteps()));
-	}
+    public static ManuallyBuildActionConstructor of() {
+        return new ManuallyBuildActionConstructor(new ArrayList<>());
+    }
 
-	public List<ManuallyBuildStep> getSteps() {
-		return Collections.unmodifiableList(steps);
-	}
+    public static ManuallyBuildActionConstructor of(ManuallyBuildAction action) {
+        return new ManuallyBuildActionConstructor(new ArrayList<>(action.getSteps()));
+    }
 
-	public ManuallyBuildActionConstructor addStep(int index, ManuallyBuildStep step) {
-		index = index + 1;
-		if (index >= steps.size()) {
-			index = steps.size() - 1;
-		}
-		if (index < 0) { // Edge case where list is empty.
-			index = 0;
-		}
+    public List<ManuallyBuildStep> getSteps() {
+        return Collections.unmodifiableList(steps);
+    }
 
-		steps.add(index, step);
-		return this;
-	}
+    public ManuallyBuildActionConstructor addStep(int index, ManuallyBuildStep step) {
+        index = index + 1;
+        if (index >= steps.size()) {
+            index = steps.size() - 1;
+        }
+        if (index < 0) { // Edge case where list is empty.
+            index = 0;
+        }
 
-	public ManuallyBuildActionConstructor removeStep(int index) {
-		if (index >= 0 && index < steps.size()) {
-			steps.remove(index);
-		}
-		return this;
-	}
+        steps.add(index, step);
+        return this;
+    }
 
-	public ManuallyBuildActionConstructor moveStepUp(int index) {
-		if (index <= 0 || index >= steps.size()) {
-			return this;
-		}
+    public ManuallyBuildActionConstructor removeStep(int index) {
+        if (index >= 0 && index < steps.size()) {
+            steps.remove(index);
+        }
+        return this;
+    }
 
-		ManuallyBuildStep tmp = steps.get(index);
-		steps.set(index, steps.get(index - 1));
-		steps.set(index - 1, tmp);
-		return this;
-	}
+    public ManuallyBuildActionConstructor moveStepUp(int index) {
+        if (index <= 0 || index >= steps.size()) {
+            return this;
+        }
 
-	public ManuallyBuildActionConstructor moveStepDown(int index) {
-		if (index < 0 || index >= steps.size()) {
-			return this;
-		}
+        ManuallyBuildStep tmp = steps.get(index);
+        steps.set(index, steps.get(index - 1));
+        steps.set(index - 1, tmp);
+        return this;
+    }
 
-		ManuallyBuildStep tmp = steps.get(index);
-		steps.set(index, steps.get(index + 1));
-		steps.set(index + 1, tmp);
-		return this;
-	}
+    public ManuallyBuildActionConstructor moveStepDown(int index) {
+        if (index < 0 || index >= steps.size()) {
+            return this;
+        }
 
-	public String generateSource() {
-		List<String> lines = Stream.concat(
-				Stream.of(ManualBuildNativeCompiler.VERSION_PREFIX + ManualBuildNativeCompiler.VERSION),
-				steps.stream().map(ManuallyBuildStep::jsonize).map(JSONUtility::jsonToSingleLineString)
-			).collect(Collectors.toList());
-		return String.join("\n", lines);
-	}
+        ManuallyBuildStep tmp = steps.get(index);
+        steps.set(index, steps.get(index + 1));
+        steps.set(index + 1, tmp);
+        return this;
+    }
 
-	private ManuallyBuildActionConstructor(List<ManuallyBuildStep> steps) {
-		this.steps = steps;
-	}
+    public String generateSource() {
+        List<String> lines = Stream.concat(
+                Stream.of(ManualBuildNativeCompiler.VERSION_PREFIX + ManualBuildNativeCompiler.VERSION),
+                steps.stream().map(ManuallyBuildStep::jsonize).map(JSONUtility::jsonToSingleLineString)
+        ).collect(Collectors.toList());
+        return String.join("\n", lines);
+    }
 }
--- src/core/webui/server/handlers/internals/tasks/manuallybuild/ActionManuallyBuildActionBuilldAction.java
+++ src/core/webui/server/handlers/internals/tasks/manuallybuild/ActionManuallyBuildActionBuilldAction.java
@@ -1,48 +1,47 @@
 package core.webui.server.handlers.internals.tasks.manuallybuild;
 
-import java.io.IOException;
-import java.util.Map;
-
-import org.apache.http.HttpRequest;
-import org.apache.http.HttpStatus;
-import org.apache.http.nio.protocol.HttpAsyncExchange;
-import org.apache.http.protocol.HttpContext;
-
 import core.languageHandler.Language;
 import core.userDefinedTask.manualBuild.ManuallyBuildActionConstructor;
 import core.userDefinedTask.manualBuild.ManuallyBuildActionConstructorManager;
 import core.webui.server.handlers.AbstractSingleMethodHttpHandler;
 import core.webui.webcommon.HttpServerUtilities;
+import org.apache.http.HttpRequest;
+import org.apache.http.HttpStatus;
+import org.apache.http.nio.protocol.HttpAsyncExchange;
+import org.apache.http.protocol.HttpContext;
+
+import java.io.IOException;
+import java.util.Map;
 
 @SuppressWarnings("SpellCheckingInspection")
 public class ActionManuallyBuildActionBuilldAction extends AbstractSingleMethodHttpHandler {
 
-	private final ManuallyBuildActionConstructorManager manuallyBuildActionConstructorManager;
+    private final ManuallyBuildActionConstructorManager manuallyBuildActionConstructorManager;
 
-	public ActionManuallyBuildActionBuilldAction(ManuallyBuildActionConstructorManager manuallyBuildActionConstructorManager) {
-		super(AbstractSingleMethodHttpHandler.POST_METHOD);
-		this.manuallyBuildActionConstructorManager = manuallyBuildActionConstructorManager;
-	}
+    public ActionManuallyBuildActionBuilldAction(ManuallyBuildActionConstructorManager manuallyBuildActionConstructorManager) {
+        super(AbstractSingleMethodHttpHandler.POST_METHOD);
+        this.manuallyBuildActionConstructorManager = manuallyBuildActionConstructorManager;
+    }
 
-	@Override
-	protected Void handleAllowedRequestWithBackend(HttpRequest request, HttpAsyncExchange exchange, HttpContext context) throws IOException {
-		Map<String, String> params = HttpServerUtilities.parseSimplePostParameters(request);
-		if (params == null) {
-			return HttpServerUtilities.prepareHttpResponse(exchange, 400, "Failed to get POST parameters.");
-		}
+    @Override
+    protected Void handleAllowedRequestWithBackend(HttpRequest request, HttpAsyncExchange exchange, HttpContext context) throws IOException {
+        Map<String, String> params = HttpServerUtilities.parseSimplePostParameters(request);
+        if (params == null) {
+            return HttpServerUtilities.prepareHttpResponse(exchange, 400, "Failed to get POST parameters.");
+        }
 
-		String id = params.get("id");
-		if (id == null || id.isEmpty()) {
-			return HttpServerUtilities.prepareHttpResponse(exchange, 400, "No builder ID provided.");
-		}
+        String id = params.get("id");
+        if (id == null || id.isEmpty()) {
+            return HttpServerUtilities.prepareHttpResponse(exchange, 400, "No builder ID provided.");
+        }
 
-		ManuallyBuildActionConstructor constructor = manuallyBuildActionConstructorManager.get(id);
-		String source = constructor.generateSource();
-		backEndHolder.setCompilingLanguage(Language.MANUAL_BUILD);
-		if (!backEndHolder.compileSourceAndSetCurrent(source, null)) {
-			return HttpServerUtilities.prepareHttpResponse(exchange, 500, "Unable to compile generated source code.");
-		}
+        ManuallyBuildActionConstructor constructor = manuallyBuildActionConstructorManager.get(id);
+        String source = constructor.generateSource();
+        backEndHolder.setCompilingLanguage(Language.MANUAL_BUILD);
+        if (!backEndHolder.compileSourceAndSetCurrent(source, null)) {
+            return HttpServerUtilities.prepareHttpResponse(exchange, 500, "Unable to compile generated source code.");
+        }
 
-		return HttpServerUtilities.prepareHttpResponse(exchange, HttpStatus.SC_OK, "");
-	}
+        return HttpServerUtilities.prepareHttpResponse(exchange, HttpStatus.SC_OK, "");
+    }
 }
