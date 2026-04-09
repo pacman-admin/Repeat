65d7c7f989b572429f68945d0b3482c2fb07f217
langdon <getpacman.gq@gmail.com>
2026-04-08 20:21:41 -0400

    Fixed bad default port (Oops!)

--- src/core/config/Constants.java
+++ src/core/config/Constants.java
@@ -24,7 +24,7 @@ import java.awt.event.KeyEvent;
  * @author HP Truong
  */
 public final class Constants {
-    public static final int DEFAULT_SERVER_PORT = 654321;
+    public static final int DEFAULT_SERVER_PORT = 54321;
     public static final String PROGRAM_VERSION = "6.0.0";
     public static final int HALT_TASK = KeyEvent.VK_ESCAPE; // This should be hardcoded, and must not be changed
     public static final String CURRENT_CONFIG_VERSION = "3.0";
