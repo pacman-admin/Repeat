a8328de96c67e9734b5f119937936d1029b5be93
langdon <getpacman.gq@gmail.com>
2026-04-13 18:26:32 -0400

    Removed useless garbage (there's an endless supply)

--- src/core/keyChain/ActionInvoker.java
+++ src/core/keyChain/ActionInvoker.java
@@ -3,7 +3,6 @@ package core.keyChain;
 import argo.jdom.JsonNode;
 import argo.jdom.JsonNodeFactories;
 import argo.jdom.JsonRootNode;
-import core.userDefinedTask.internals.SharedVariablesSubscription;
 import utilities.json.IJsonable;
 import utilities.json.JSONUtility;
 
@@ -19,20 +18,12 @@ import java.util.logging.Logger;
 public final class ActionInvoker implements IJsonable {
 
     private static final Logger LOGGER = Logger.getLogger(ActionInvoker.class.getName());
-    private final Set<ActivationPhrase> phrases;
-    private final Set<SharedVariablesActivation> variables;
     private Set<KeyChain> hotkeys;
     private Set<MouseGesture> mouseGestures;
-    private Set<KeySequence> keySequences;
-    private GlobalActivation globalActivation;
 
     private ActionInvoker(Builder builder) {
         hotkeys = builder.hotkeys;
         mouseGestures = builder.mouseGestures;
-        keySequences = builder.keySequences;
-        phrases = builder.phrases;
-        variables = builder.variables;
-        globalActivation = builder.globalActivation;
     }
 
     /**
@@ -44,10 +35,7 @@ public final class ActionInvoker implements IJsonable {
     public static ActionInvoker parseJSON(JsonNode node) {
         try {
             List<JsonNode> hotkeysNode = node.getArrayNode("hotkey");
-            List<JsonNode> keySequenceNodes = node.isArrayNode("key_sequence") ? node.getArrayNode("key_sequence") : new ArrayList<>();
             List<JsonNode> mouseGestureNode = node.getArrayNode("mouse_gesture");
-            List<JsonNode> phrasesNodes = node.isArrayNode("phrases") ? node.getArrayNode("phrases") : new ArrayList<>();
-            List<JsonNode> variablesNodes = node.getArrayNode("variables");
 
             Set<KeyChain> keyChains = new HashSet<>();
             for (JsonNode hotkeyNode : hotkeysNode) {
@@ -59,52 +47,13 @@ public final class ActionInvoker implements IJsonable {
                 }
             }
 
-            Set<KeySequence> keySequences = new HashSet<>();
-            for (JsonNode keySequenceNode : keySequenceNodes) {
-                KeySequence newkeySequence = KeySequence.parseJSON(keySequenceNode.getArrayNode());
-                if (newkeySequence == null) {
-                    LOGGER.log(Level.WARNING, "Cannot parse key chain " + keySequenceNode);
-                } else {
-                    keySequences.add(newkeySequence);
-                }
-            }
 
             Set<MouseGesture> gestures = MouseGesture.parseJSON(mouseGestureNode);
 
-            Set<ActivationPhrase> phrases = new HashSet<>();
-            for (JsonNode phraseNode : phrasesNodes) {
-                ActivationPhrase phrase = ActivationPhrase.parseJSON(phraseNode);
-                if (phrase == null) {
-                    LOGGER.log(Level.WARNING, "Cannot parse phrase " + phraseNode);
-                } else {
-                    phrases.add(phrase);
-                }
-            }
-
-            Set<SharedVariablesActivation> variables = new HashSet<>();
-            for (JsonNode variableNode : variablesNodes) {
-                SharedVariablesActivation variable = SharedVariablesActivation.parseJSON(variableNode);
-                if (variable == null) {
-                    LOGGER.log(Level.WARNING, "Cannot parse variable node " + variableNode);
-                } else {
-                    variables.add(variable);
-                }
-            }
-
-            GlobalActivation globalActivation = node.isNode("global_activation")
-                    ? GlobalActivation.parseJSON(node.getNode("global_activation"))
-                    : GlobalActivation.newBuilder().build();
-            if (globalActivation == null) {
-                globalActivation = GlobalActivation.newBuilder().build();
-            }
 
             return ActionInvoker.newBuilder()
                     .withHotKeys(keyChains)
-                    .withKeySequence(keySequences)
                     .withMouseGestures(gestures)
-                    .withPhrases(phrases)
-                    .withVariables(variables)
-                    .withGlobalActivation(globalActivation)
                     .build();
         } catch (Exception e) {
             LOGGER.log(Level.WARNING, "Exception while parsing task activation.", e);
@@ -177,120 +126,16 @@ public final class ActionInvoker implements IJsonable {
         }
     }
 
-    /**
-     * @return set of key sequences associated with this activation entity.
-     */
-    public Set<KeySequence> getKeySequences() {
-        if (keySequences == null) {
-            return new HashSet<>();
-        }
-
-        return keySequences;
-    }
-
-    /**
-     * @param keySequences set of key sequences to set.
-     */
-    private void setKeySequences(Set<KeySequence> keySequences) {
-        this.keySequences = new HashSet<>();
-        this.keySequences.addAll(keySequences);
-    }
-
-    /**
-     * @return an arbitrary {@link KeySequence} from the set of gestures, or null if the set is empty.
-     */
-    public KeySequence getFirstKeySequence() {
-        Set<KeySequence> keySequences = getKeySequences();
-        if (keySequences.isEmpty()) {
-            return null;
-        } else {
-            return keySequences.iterator().next();
-        }
-    }
-
-    /**
-     * @return set of phrases associated with this activation entity.
-     */
-    public Set<ActivationPhrase> getPhrases() {
-        if (phrases == null) {
-            return new HashSet<>();
-        }
-
-        return phrases;
-    }
-
-    /**
-     * @param phrases set of key phrases to set.
-     */
-    private void setPhrases(Set<ActivationPhrase> phrases) {
-        this.phrases.clear();
-        this.phrases.addAll(phrases);
-    }
-
-    /**
-     * @return set of variables associated with this activation entity.
-     */
-    public Set<SharedVariablesActivation> getVariables() {
-        if (variables == null) {
-            return new HashSet<>();
-        }
-
-        return variables;
-    }
-
-    private void setVariables(Set<SharedVariablesActivation> variables) {
-        this.variables.clear();
-        this.variables.addAll(variables);
-    }
-
-    /**
-     * @return an arbitrary variable from the set of variables, or null if the set is empty.
-     */
-    public SharedVariablesActivation getFirstVariable() {
-        Set<SharedVariablesActivation> variables = getVariables();
-        if (variables.isEmpty()) {
-            return null;
-        } else {
-            return variables.iterator().next();
-        }
-    }
-
-    /**
-     * @return the global activation configuration for this activation.
-     */
-    public GlobalActivation getGlobalActivation() {
-        if (globalActivation == null) {
-            return GlobalActivation.newBuilder().build();
-        }
-        return globalActivation;
-    }
-
-    /**
-     * @param globalActivation configuration to set.
-     */
-    private void setGlobalActivation(GlobalActivation globalActivation) {
-        this.globalActivation = globalActivation;
-    }
-
-    /**
-     * Copy the content of the other {@link ActionInvoker} to this object.
-     *
-     * @param other other task activation whose content will be copied from.
-     */
     public void copy(ActionInvoker other) {
         setHotKeys(other.getHotkeys());
         setMouseGestures(other.getMouseGestures());
-        setKeySequences(other.getKeySequences());
-        setPhrases(other.getPhrases());
-        setVariables(other.getVariables());
-        setGlobalActivation(other.getGlobalActivation());
     }
 
     /**
      * Check if this activation is empty (i.e. no event for activation).
      */
     public boolean isEmpty() {
-        return getHotkeys().isEmpty() && getMouseGestures().isEmpty() && getKeySequences().isEmpty() && getPhrases().isEmpty();
+        return getHotkeys().isEmpty() && getMouseGestures().isEmpty();
     }
 
     /**
@@ -301,27 +146,9 @@ public final class ActionInvoker implements IJsonable {
         if (!getHotkeys().isEmpty()) {
             return "{" + getHotkeys().iterator().next().toString() + "}";
         }
-        if (!getKeySequences().isEmpty()) {
-            return "<" + getKeySequences().iterator().next().toString() + ">";
-        }
-        if (!getPhrases().isEmpty()) {
-            return "(" + getPhrases().iterator().next().toString() + ")";
-        }
         if (!getMouseGestures().isEmpty()) {
             return "[" + getMouseGestures().iterator().next().toString() + "]";
         }
-        if (!getVariables().isEmpty()) {
-            SharedVariablesSubscription var = getVariables().iterator().next().getVariable();
-            if (var.isAll()) {
-                return "-(all)-";
-            }
-            if (var.isAllForNamespace()) {
-                return "-(" + var.getNamespace() + ") - (all)-";
-            }
-
-            return "-(" + var.getNamespace() + ") - (" + var.getName() + ")-";
-        }
-
         return new KeyChain().toString();
     }
 
@@ -329,22 +156,14 @@ public final class ActionInvoker implements IJsonable {
     public JsonRootNode jsonize() {
         return JsonNodeFactories.object(
                 JsonNodeFactories.field("hotkey", JsonNodeFactories.array(JSONUtility.listToJson(getHotkeys()))),
-                JsonNodeFactories.field("key_sequence", JsonNodeFactories.array(JSONUtility.listToJson(getKeySequences()))),
-                JsonNodeFactories.field("mouse_gesture", JsonNodeFactories.array(JSONUtility.listToJson(getMouseGestures()))),
-                JsonNodeFactories.field("phrases", JsonNodeFactories.array(JSONUtility.listToJson(getPhrases()))),
-                JsonNodeFactories.field("variables", JsonNodeFactories.array(JSONUtility.listToJson(getVariables()))),
-                JsonNodeFactories.field("global_activation", globalActivation.jsonize()));
+                JsonNodeFactories.field("mouse_gesture", JsonNodeFactories.array(JSONUtility.listToJson(getMouseGestures()))));
     }
 
     @Override
     public int hashCode() {
         return Objects.hash(
-                getGlobalActivation(),
                 getHotkeys(),
-                getKeySequences(),
-                getMouseGestures(),
-                getPhrases(),
-                getVariables());
+                getMouseGestures());
     }
 
     @Override
@@ -359,12 +178,8 @@ public final class ActionInvoker implements IJsonable {
             return false;
         }
         ActionInvoker other = (ActionInvoker) obj;
-        return getGlobalActivation().equals(other.getGlobalActivation())
-                && getHotkeys().equals(other.getHotkeys())
-                && getKeySequences().equals(other.getKeySequences())
-                && getMouseGestures().equals(other.getMouseGestures())
-                && getPhrases().equals(other.getPhrases())
-                && getVariables().equals(other.getVariables());
+        return getHotkeys().equals(other.getHotkeys())
+                && getMouseGestures().equals(other.getMouseGestures());
     }
 
     /**
@@ -373,18 +188,10 @@ public final class ActionInvoker implements IJsonable {
     public static final class Builder {
         private final Set<KeyChain> hotkeys;
         private final Set<MouseGesture> mouseGestures;
-        private final Set<KeySequence> keySequences;
-        private final Set<ActivationPhrase> phrases;
-        private final Set<SharedVariablesActivation> variables;
-        private GlobalActivation globalActivation;
 
         private Builder() {
             hotkeys = new HashSet<>();
             mouseGestures = new HashSet<>();
-            keySequences = new HashSet<>();
-            phrases = new HashSet<>();
-            variables = new HashSet<>();
-            globalActivation = GlobalActivation.newBuilder().build();
         }
 
         public Builder addHotKeys(KeyChain... keys) {
@@ -418,62 +225,6 @@ public final class ActionInvoker implements IJsonable {
         public Builder withMouseGestures(Collection<MouseGesture> gestures) {
             this.mouseGestures.clear();
             this.mouseGestures.addAll(gestures);
-            return this;
-        }
-
-        public Builder addKeySequence(KeySequence... keySequences) {
-            this.keySequences.addAll(Arrays.asList(keySequences));
-            return this;
-        }
-
-        public Builder withKeySequence(KeySequence keySequences) {
-            this.keySequences.clear();
-            this.keySequences.add(keySequences);
-            return this;
-        }
-
-        public Builder withKeySequence(Collection<KeySequence> keySequences) {
-            this.keySequences.clear();
-            this.keySequences.addAll(keySequences);
-            return this;
-        }
-
-        public Builder addPhrases(ActivationPhrase... phrases) {
-            this.phrases.addAll(Arrays.asList(phrases));
-            return this;
-        }
-
-        public Builder withPhrase(ActivationPhrase phrase) {
-            this.phrases.clear();
-            this.phrases.add(phrase);
-            return this;
-        }
-
-        public Builder withPhrases(Collection<ActivationPhrase> phrases) {
-            this.phrases.clear();
-            this.phrases.addAll(phrases);
-            return this;
-        }
-
-        public Builder addVariables(SharedVariablesActivation... variables) {
-            this.variables.addAll(Arrays.asList(variables));
-            return this;
-        }
-
-        public Builder withVariable(SharedVariablesActivation variable) {
-            this.variables.clear();
-            this.variables.add(variable);
-            return this;
-        }
-
-        public Builder withVariables(Collection<SharedVariablesActivation> variables) {
-            this.variables.clear();
-            this.variables.addAll(variables);
-            return this;
-        }
-
-        public Builder withGlobalActivation(GlobalActivation globalActivation) {
-            this.globalActivation = globalActivation;
             return this;
         }
 
