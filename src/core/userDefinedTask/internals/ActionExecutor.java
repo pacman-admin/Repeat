17a4e15aff1283b161e2ba4dcffa8133a463b854
langdon <getpacman.gq@gmail.com>
2026-04-28 13:01:09 -0400

    Prevented many tasks from destroying computer.

--- src/core/userDefinedTask/internals/ActionExecutor.java
+++ src/core/userDefinedTask/internals/ActionExecutor.java
@@ -3,13 +3,8 @@ package core.userDefinedTask.internals;
 import core.controller.Core;
 import core.userDefinedTask.UserDefinedAction;
 
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -18,7 +13,8 @@ public final class ActionExecutor {
     private static final Logger LOGGER = Logger.getLogger(ActionExecutor.class.getName());
     private final Core core;
     private final ExecutorService executor = Executors.newSingleThreadExecutor();
-    private final List<Future<?>> executions = new ArrayList<>();
+    //    private final List<Future<?>> executions = new ArrayList<>();
+    private Future<?> execution;
 
     public ActionExecutor(Core controller) {
         this.core = controller;
@@ -45,32 +41,35 @@ public final class ActionExecutor {
      * @param action  action to execute
      */
     public void startExecutingAction(ActionExecutionRequest request, UserDefinedAction action) {
-        if (action == null) {
-            throw new IllegalArgumentException("Nothing to run.");
-        }
-        if(executor.isShutdown()) return;
-        executions.add(executor.submit(() -> {
+        if (execution.isDone()) {
             try {
-                for (int i = 0; i < request.getRepeatCount(); i++) {
-                    action.trackedAction(core);
-                    Thread.sleep(request.getDelayMsBetweenRepeat());
-                }
-            } catch (InterruptedException e) {
-                LOGGER.info("Task ended prematurely");
-            } catch (Exception e) {
-                LOGGER.log(Level.WARNING, "Exception while executing task " + action.getName(), e);
+                execution = executor.submit(() -> {
+                    try {
+                        for (int i = 0; i < request.getRepeatCount(); i++) {
+                            action.trackedAction(core);
+                            Thread.sleep(request.getDelayMsBetweenRepeat());
+                        }
+                    } catch (InterruptedException e) {
+                        LOGGER.info("Task ended prematurely");
+                    } catch (Exception e) {
+                        LOGGER.log(Level.WARNING, "Exception while executing task " + action.getName(), e);
+                    }
+                });
+            } catch (RejectedExecutionException ignored) {
+            } catch (NullPointerException ignored) {
+                LOGGER.warning("Nothing to run");
             }
-        }));
-        executions.removeIf(Future::isDone);
+        }
     }
 
     /**
      * Interrupt all currently executing tasks, and clear the record of all executing tasks
      */
     public void haltAllTasks() {
-        executions.forEach(task -> task.cancel(true));
-        LOGGER.info("Halting all tasks...");
-        executions.clear();
+//        executions.forEach(task -> task.cancel(true));
+        execution.cancel(true);
+        LOGGER.info("Halting current task...");
+//        executions.clear();
     }
 
     public void shutdown() {
